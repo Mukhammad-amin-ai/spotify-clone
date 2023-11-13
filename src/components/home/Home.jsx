72134c950b92b8d38c9f4eaf1c95b47b74b391fb
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-11-13 21:31:55 +0500

    small changes from left side

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import './home.css'
+import Collapse from '../../assets/svg-elements/Collapse'
+
 
 export default function Home() {
     return (
@@ -25,7 +27,34 @@ export default function Home() {
                         </nav>
                         <div className="library">
                             <div className="library-navigatin">
-
+                                <div className="navigation-title">
+                                    <div className="title-left">
+                                        <span className='icon'>
+                                            <Collapse />
+                                        </span>
+                                        Your Library
+                                    </div>
+                                    <div className="title-right">
+                                        <i className="fa-solid fa-plus"></i>
+                                        <i className="fa-solid fa-arrow-right"></i>
+                                    </div>
+                                </div>
+                                <div className="navigation-info">
+                                    <span className='nav-filter'>
+                                        Artist
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="library-list">
+                                <header>
+                                    <button className='search-btn'>
+                                        <i className="fa-solid fa-magnifying-glass"></i>
+                                    </button>
+                                    <button className='resent-btn'>
+                                        Recents
+                                        <i className="fa-solid fa-list-ul"></i>
+                                    </button>
+                                </header>
                             </div>
                         </div>
                     </div>
